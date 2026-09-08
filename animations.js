e63c11fb8196cb8d5265e671f7a12a0b97a16638
lingalamanasa <lingalamanasa123@gmail.com>
2026-09-08 15:18:45 +0530

    perf(preloader): further reduce preloader timer to 0.32s with 0.38s curtain reveal

--- animations.js
+++ animations.js
@@ -95,7 +95,7 @@ function initStacklyPreloader() {
   if (typeof gsap !== 'undefined') {
     gsap.to(obj, {
       val: 100,
-      duration: 0.55,
+      duration: 0.32,
       ease: 'power2.inOut',
       onUpdate: () => {
         const rounded = Math.round(obj.val);
@@ -111,15 +111,15 @@ function initStacklyPreloader() {
         setTimeout(() => {
           preloader.classList.add('is-hidden');
           if (typeof AOS !== 'undefined') AOS.refresh();
-        }, 600);
+        }, 400);
       }
     });
   } else {
     // Graceful fallback
     setTimeout(() => {
       preloader.classList.add('loaded');
-      setTimeout(() => preloader.classList.add('is-hidden'), 600);
-    }, 500);
+      setTimeout(() => preloader.classList.add('is-hidden'), 400);
+    }, 250);
   }
 }
 
